76e8c99e551414eb6b3509bc710c4aea0a47ed5b
ClintonWeeYuan <clinton_wee@hotmail.com>
2022-08-13 20:21:52 +0800

    Changed Login Form Labels

--- pages/login.tsx
+++ pages/login.tsx
@@ -28,15 +28,13 @@ export default function Login() {
     <div className="max-w-lg h-screen flex justify-center items-center mx-auto">
       <form onSubmit={handleSubmit} className="w-full">
         <div className="mb-6">
-          <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
-            email</label>
+          <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Username</label>
           <input onChange={(e) => {setUsername(e.target.value)}} type="text" id="username"
                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                  placeholder="username" required/>
         </div>
         <div className="mb-6">
-          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
-            password</label>
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
           <input onChange={(e) => {setPassword(e.target.value)}} type="password" id="password"
                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                  required/>
