b5671d2c17fcb0dca2ff12722d329b8bd6a356bb
ClintonWeeYuan <clinton_wee@hotmail.com>
2022-08-13 20:18:00 +0800

    Admin page now checks to see if admin is logged in

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,8 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
+import useUser from "../utils/useUser";
 
 const Admin: NextPage = () => {
+  const { user } = useUser({
+    redirectTo: "/login",
+  });
+
   return (
     <div>
       <Head>
