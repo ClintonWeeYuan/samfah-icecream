595ff51d2a99b4ca12037e6f0135bd99a8238532
ClintonWeeYuan <clinton_wee@hotmail.com>
2022-08-16 08:32:07 +0800

    Added success toast and spinners to place order page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -5,25 +5,39 @@ import OrderTable from "../components/OrderTable";
 import {useBasketContext} from "../components/BasketContext";
 import axios from "axios"
 import {useState} from "react"
+import Spinner from "../components/Spinner"
+import SuccessToast from "../components/SuccessToast";
 
 const Checkout: NextPage = () => {
   const basket = useBasketContext().basket;
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [openToast, setOpenToast] = useState<boolean>(false);
 
   async function addOrder() {
-    const items = basket.map((item) => ({orderName: item.name, orderQuantity: item.quantity, price: item.price}))
-    const data = {
-      orders: items,
-      name: name,
-      email: email,
-    }
 
-    const res = await axios.post("/api/add", data);
-    console.log(res);
+    try{
+      const items = basket.map((item) => ({orderName: item.name, orderQuantity: item.quantity, price: item.price}))
+      const data = {
+        orders: items,
+        name: name,
+        email: email,
+      }
+      setLoading(true);
+      const res = await axios.post("/api/add", data);
+      setOpenToast(true);
+      const closeToast = setTimeout(() => {setOpenToast(false)} , 3000)
+      setLoading(false);
+      console.log(res);
+    } catch(e){
+      console.log(e);
+      setLoading(false);
+    }
   }
   return (
     <div>
+      <SuccessToast openToast={openToast}/>
       <ShopHeader/>
       <div className="p-4 md:px-20 md:py-10 grid grid-cols-1 md:grid-cols-2 gap-8  w-full">
         <div className="cols-span-2 md:cols-span-1">
@@ -35,8 +49,9 @@ const Checkout: NextPage = () => {
           <OrderTable/>
           <div className="mt-4 flex justify-center md:justify-end">
             <button onClick={addOrder}
-              className="inline-flex justify-center items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              Place Order
+                    disabled={loading}
+              className="inline-flex w-full justify-center items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              {loading ? <Spinner/> : "PLACE ORDER"}
             </button>
           </div>
         </div>
